471931c0562bce13d1856d08861063ae0df3f03e
jerzy <jerzy.z.bialowiezy@proton.me>
2023-08-08 16:36:03 +0200

    wizard2

--- games/ultimate_wizard/player.py
+++ games/ultimate_wizard/player.py
@@ -6,6 +6,6 @@ class Player(Sprite):
     initial_spells = 10
 
     def __init__(self, x, y):
-        Sprite.__init__(self, x, y)
+        Sprite.__init__(self, x, y, "wizard")
         self.score = 0
         self.spells = Player.initial_spells
