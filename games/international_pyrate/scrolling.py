1f1a11308f0c759576a5af5f6ef0b485cb8ea192
kdrzazga <kdrzazga@luxoft.com>
2023-08-10 06:34:24 +0200

    ik2

--- games/international_pyrate/scrolling.py
+++ games/international_pyrate/scrolling.py
@@ -10,12 +10,11 @@ ORANGE = (255, 128, 64)
 WHITE = (250, 250, 250)
 YELLOW = (238, 238, 119)
 
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+#screen_width = 800
+#screen_height = 600
+#screen = pygame.display.set_mode((screen_width, screen_height))
 
-def scroll(screen):
-    #screen.fill((0, 0, 0))  # Black background
+def info(screen):
     font_path = os.path.join("resources", "karate.ttf")
     caption_font = ImageFont.truetype(font_path, 15)
     caption_text_height = 13
@@ -32,9 +31,4 @@ def scroll(screen):
 
     caption_surface = pygame.image.fromstring(caption_image.tobytes(), caption_image.size, caption_image.mode)
     screen.blit(caption_surface, (0, 0))
-
-    #screen.blit(caption_surface, (background_x, 0))
-
-    #background_x -= scroll_speed
-    #if background_x <= -caption_surface.get_width():
-    #    background_x = 0
+    
