3fd8b8637ff78bb6c2d160caff4d91e20cabf5a3
jerzy <jerzy.z.bialowiezy@proton.me>
2023-11-19 09:18:45 +0100

    questions fix

--- games-anims/retro-quiz/questions.py
+++ games-anims/retro-quiz/questions.py
@@ -21,7 +21,11 @@ class QuestionsFactory:
         questions_set.append(Question('a) Punisher (MAME)', 'b) Fire And Ice', 'c) Budokan', 'd) Mortal Kombat', 'D', r + 'mkbgnd.PNG', r + 'mkbgnd_full.PNG', r + 'mk.mp4'))
         questions_set.append(Question('a) Street Fighter 2 (Amiga)', 'b) Superfrog', 'c) Cadillacs & Dinosaurs', 'd) Final Fight', 'C', r + 'bgnd.PNG', r + 'bgnd_full.PNG', r + 'kadilaki.mpg'))
         questions_set.append(Question('a) Franko', 'b) Miecze Valdgira', "c) Teenagent", 'd) Fort Apache', 'A', r + 'fbgnd.PNG', r + 'fbgnd_full.PNG', r + 'franko_intro.mp4'))
-        questions_set.append(Question('a) Franko', 'b) Golden Axe', "c) Flashback", 'd) Bloodwych', 'A', r + 'fbgnd.PNG', r + f'bgnd_full.PNG', r + 'franko_intro.mp4'))
+        questions_set.append(Question('a) Warriors of Fate', 'b) Punisher', "c) Flashback", 'd) Bloodwych', 'B', r + 'pnshbgnd_full.PNG', r + 'pnshbgnd_full.PNG', r + 'punisher.mp4'))
+        questions_set.append(
+            Question('a) Warriors of Fate', 'b) Punisher', "c) Flashback", 'd) Bloodwych', 'B', r + 'pnshbgnd_full.PNG',
+                     r + 'pnshbgnd_full.PNG', r + 'punisher.mp4'))
+
         questions_set.append(Question('a) Galaga Deluxe', 'b) Beavers (Amiga)', "c) Blinky's Scary School", 'd) Bloodwych', 'A', r + 'bgnd.PNG', r + 'bgnd_full.PNG', r + 'kadilaki.mpg'))
         questions_set.append(Question('a) Elvira II: The Jaws of Cerberus', 'b) Hardball! (Amiga)', "c) Ghosts'n'Goblins", 'd) Gods', 'A', r + 'fbgnd.PNG', r + 'fbgnd_full.PNG', r + 'franko_intro.mp4'))
 
