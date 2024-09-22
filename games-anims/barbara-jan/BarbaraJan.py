815edac3999f404668e18d36b6b2581e72586ed9
kdrzazga <kdrzazga@luxoft.com>
2024-09-22 07:48:05 +0200

    AI refactor

--- games-anims/barbara-jan/BarbaraJan.py
+++ games-anims/barbara-jan/BarbaraJan.py
@@ -34,29 +34,33 @@ class BarbaraJan(arcade.Window):
         self.karateka_fighter = Karateka(self.board.arena_offset)
         self.random_draw_reset = True
         self.time = 0
-        self.key_state = {key: False for key in (
+        self.key_state = self.initialize_key_state()
+
+    def initialize_key_state(self):
+        return {key: False for key in (
             KEYS["HONDA_RIGHT"],
             KEYS["HONDA_LEFT"],
             KEYS["KARATEKA_RIGHT"],
             KEYS["KARATEKA_LEFT"],
             KEYS["CHANGE_COLOR"],
             KEYS["QUIT"],
+            *KEYS["HONDA_PUNCH"],
+            *KEYS["KARATEKA_PUNCH"]
         )}
-        for key in KEYS["HONDA_PUNCH"]:
-            self.key_state[key] = False
-        for key in KEYS["KARATEKA_PUNCH"]:
-            self.key_state[key] = False
 
     def on_draw(self):
         arcade.start_render()
         if GameState.INTRO == self.game_on:
             self.intro.show()
         else:
-            self.board.draw()
-            self.draw_hp(self.honda_fighter)
-            self.draw_hp(self.karateka_fighter)
-            self.karateka_fighter.draw()
-            self.honda_fighter.draw()
+            self.draw_game()
+
+    def draw_game(self):
+        self.board.draw()
+        self.draw_hp(self.honda_fighter)
+        self.draw_hp(self.karateka_fighter)
+        self.karateka_fighter.draw()
+        self.honda_fighter.draw()
 
     def draw_hp(self, fighter: Fighter):
         radius = 13
@@ -64,8 +68,7 @@ class BarbaraJan(arcade.Window):
         shift = Constants.SCREEN_WIDTH - 160 if isinstance(fighter, Karateka) else 26
 
         for i in range(fighter.hp):
-            row = i // 3
-            col = i % 3
+            row, col = divmod(i, 3)
 
             center_x = shift + col * (radius * 2 + 38)
             center_y = Constants.SCREEN_HEIGHT - row * (radius * 2 + 21) - 22
@@ -73,7 +76,7 @@ class BarbaraJan(arcade.Window):
 
     def update(self, delta_time):
         self.time += delta_time
-        if self.time > 13 and self.game_on == GameState.INTRO:
+        if self.time > 1 and self.game_on == GameState.INTRO:
             self.game_on = GameState.FIGHT
         # print(self.time, end='\t')
 
@@ -90,37 +93,37 @@ class BarbaraJan(arcade.Window):
         self.board.dialog = False
 
         if self.game_on == GameState.FIGHT:
-            if self.time < 15:
-                self.board.dialog = True
-                self.board.message_ptr = Data.fight_call
-
-            t1 = math.floor(self.time) % 97
-
-            if 19 < t1 < 28:
-                self.board.dialog = True
-                self.board.message_ptr = Data.honda_insult
-
-            t2 = math.floor(self.time) % 35
-
-            if 30 < t2 < 33:
-                self.board.dialog = True
-                if self.random_draw_reset:
-                    self.board.message_ptr = Data.get_random()
-                self.random_draw_reset = False
-            else:
-                self.random_draw_reset = True
+            self.show_fight_dialogue()
 
         elif self.game_on == GameState.KO_HONDA or self.game_on == GameState.KO_KARATEKA:
-            self.board.dialog = True
-            self.board.message_ptr = Data.fight_over
-
-            print(f'Time = {self.time}')
-            if math.floor(self.time) % 11 == 0:
-                self.board.game_on = GameState.END
+            self.show_ko_dialogue()
         else:
             self.board.dialog = True
             self.board.message_ptr = Data.reward
 
+    def show_ko_dialogue(self):
+        self.board.dialog = True
+        self.board.message_ptr = Data.fight_over
+        print(f'Time = {self.time}')
+        if math.floor(self.time) % 11 == 0:
+            self.board.game_on = GameState.END
+
+    def show_fight_dialogue(self):
+        if self.time < 15:
+            self.board.dialog = True
+            self.board.message_ptr = Data.fight_call
+        t1 = math.floor(self.time) % 97
+        if 19 < t1 < 28:
+            self.board.dialog = True
+            self.board.message_ptr = Data.honda_insult
+        t2 = math.floor(self.time) % 35
+        if 30 < t2 < 33:
+            self.board.dialog = True
+            if self.random_draw_reset:
+                self.board.message_ptr = Data.get_random()
+            self.random_draw_reset = False
+        else:
+            self.random_draw_reset = True
 
     def handle_movement(self):
 
@@ -156,20 +159,23 @@ class BarbaraJan(arcade.Window):
 
         if self.game_on == GameState.FIGHT:
 
-            if key in KEYS["HONDA_PUNCH"]:
-                if self.honda_fighter.state != "dead":
-                    self.honda_fighter.start_punch()
+            if key in KEYS["HONDA_PUNCH"] and self.honda_fighter.state != "dead":
+                self.honda_fighter.start_punch()
 
-                    if check_distance(self.karateka_fighter.x, self.honda_fighter.x,
-                                      self.karateka_fighter.stand_image.width, self.honda_fighter.stand_image.width):
-                        self.game_on = self.karateka_fighter.receive_hit()
+                if check_distance(self.karateka_fighter.x, self.honda_fighter.x,
+                                  self.karateka_fighter.stand_image.width, self.honda_fighter.stand_image.width):
+                    self.game_on = self.karateka_fighter.receive_hit()
 
-            if key in KEYS["KARATEKA_PUNCH"]:
-                if self.karateka_fighter.state != "dead":
-                    self.karateka_fighter.start_punch()
-                    if check_distance(self.karateka_fighter.x, self.honda_fighter.x,
-                                      self.karateka_fighter.stand_image.width, self.honda_fighter.stand_image.width):
-                        self.game_on = self.honda_fighter.receive_hit()
+            if key in KEYS["KARATEKA_PUNCH"] and self.karateka_fighter.state != "dead":
+                self.karateka_fighter.start_punch()
+                if check_distance(self.karateka_fighter.x, self.honda_fighter.x,
+                                  self.karateka_fighter.stand_image.width, self.honda_fighter.stand_image.width):
+                    self.game_on = self.honda_fighter.receive_hit()
+
+    def check_hit(self, target_fighter: Fighter, attacker_fighter: Fighter):
+        if check_distance(target_fighter.x, attacker_fighter.x,
+                          target_fighter.stand_image.width, attacker_fighter.stand_image.width):
+            self.game_on = target_fighter.receive_hit()
 
     def on_key_release(self, key, modifiers):
         if self.honda_fighter.state == "dead" or self.karateka_fighter.state == "dead":
