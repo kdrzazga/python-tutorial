700ad8d29b9b06589610434bb3577e4b2b135538
kdrzazga <krzysztof.drzazga@tutanota.com>
2026-04-21 14:08:23 +0200

    Tunnel refactor

--- games-anims/tunnel/main.py
+++ games-anims/tunnel/main.py
@@ -1,6 +1,7 @@
 import arcade
 import random
 import math
+from time import time
 
 WIDTH = 800
 HEIGHT = 600
@@ -9,11 +10,15 @@ NUM_DOTS = 400
 
 class Dot:
     def __init__(self):
+        self.speed = 0.0
+        self.size = 1
+        self.y = WIDTH / 2
+        self.x = HEIGHT / 2
         self.reset()
 
     def reset(self):
         angle = random.uniform(0, 2 * math.pi)
-        radius = random.uniform(300, 600)
+        radius = random.uniform(30, min(WIDTH, HEIGHT))
         self.x = WIDTH / 2 + math.cos(angle) * radius
         self.y = HEIGHT / 2 + math.sin(angle) * radius
         self.size = 1
@@ -34,20 +39,38 @@ class Dot:
             self.reset()
 
 
+class TunnelEffect:
+
+    def __init__(self, enlarge_delay):
+        self.dots = [Dot() for _ in range(NUM_DOTS)]
+        self.dot_size = 1
+        self.enlarge_time = time() + enlarge_delay
+
+    def draw(self):
+        for dot in self.dots:
+            arcade.draw_circle_filled(dot.x, dot.y, self.dot_size, arcade.color.WHITE)
+
+    def update(self):
+        print(self.enlarge_time, time())
+        if self.enlarge_time < time() and self.dot_size < 0.12*WIDTH:
+            self.dot_size += 0.05 + 0.01*self.dot_size**1.2
+
+        for dot in self.dots:
+            dot.update()
+
+
 class TunnelWindow(arcade.Window):
     def __init__(self):
         super().__init__(WIDTH, HEIGHT, "Tunnel Effect")
         arcade.set_background_color(arcade.color.BLACK)
-        self.dots = [Dot() for _ in range(NUM_DOTS)]
+        self.effect = TunnelEffect(8)
 
     def on_draw(self):
         self.clear()
-        for dot in self.dots:
-            arcade.draw_circle_filled(dot.x, dot.y, dot.size, arcade.color.WHITE)
+        self.effect.draw()
 
     def on_update(self, delta_time):
-        for dot in self.dots:
-            dot.update()
+        self.effect.update()
 
 
 if __name__ == "__main__":
