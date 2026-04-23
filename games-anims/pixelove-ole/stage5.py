99c4831034aa2cd4e8c519c0ffa8ec7a6dc855c0
kdrzazga <krzysztof.drzazga@tutanota.com>
2026-04-23 09:38:06 +0200

    computers

--- games-anims/pixelove-ole/stage5.py
+++ games-anims/pixelove-ole/stage5.py
@@ -1,7 +1,10 @@
 import arcade
 import sys
+import os
 
 from datetime import datetime
+
+from arcade import Rect
 from arcade.color import CYAN, ORANGE, WHITE, YELLOW, GREEN, ARCADE_YELLOW, AQUA, BLACK
 
 from lib.tunnel import TunnelEffect
@@ -13,7 +16,11 @@ class Stage5:
 	START_TIMER = 700
 
 	def __init__(self):
-		self.logo = arcade.load_texture("res/1.jpg")
+		self.c64 = arcade.load_texture("res/computers/c64.png")
+		self.atari = arcade.load_texture("res/computers/atari.png")
+		self.a500 = arcade.load_texture("res/computers/a500.png")
+		self.amstrad = arcade.load_texture("res/computers/amstrad.png")
+		self.nes = arcade.load_texture("res/computers/nes.png")
 		arcade.set_background_color(arcade.color.BLACK)
 		self.effect = TunnelEffect(9)
 
@@ -26,43 +33,66 @@ class Stage5:
 		t = timer - Stage5.START_TIMER
 		duration = 11
 		print(t)
+		x = 0.5 * Globals.WIDTH
+		y = Globals.HEIGHT - 82
+
 		if 2 < t < 2 + duration:
-			arcade.draw_text("Commodore 64", 0.5 * Globals.WIDTH, Globals.HEIGHT - 82, color=CYAN, font_size=60,
-		                 anchor_x="center")
+			arcade.draw_text("Commodore 64", x, y, color=CYAN, font_size=60, anchor_x="center")
+			self.draw_pic(self.c64)
 
 		elif 2 + duration + 2 < t < 2 + 2*duration + 2:
-			arcade.draw_text("Atari 800", 0.5 * Globals.WIDTH, Globals.HEIGHT - 82, color=ORANGE, font_size=60,
-		                 anchor_x="center")
+			arcade.draw_text("Atari 800XL", x, y, color=ORANGE, font_size=60, anchor_x="center")
+			self.draw_pic(self.atari)
 
 		elif 2 + 2*duration + 4 < t < 2 + 3*duration + 2:
-			arcade.draw_text("Amiga 500", 0.5 * Globals.WIDTH, Globals.HEIGHT - 82, color=WHITE, font_size=60,
-		                 anchor_x="center")
+			arcade.draw_text("Amiga 500", x, y, color=WHITE, font_size=60, anchor_x="center")
+			self.draw_pic(self.a500)
 
 		elif 2 + 3*duration + 4 < t < 2 + 4*duration + 2:
-			arcade.draw_text("Amstrad", 0.5 * Globals.WIDTH, Globals.HEIGHT - 82, color=YELLOW, font_size=60,
-		                 anchor_x="center")
+			arcade.draw_text("Amstrad", x, y, color=YELLOW, font_size=60, anchor_x="center")
+			self.draw_pic(self.amstrad)
 
 		elif 2 + 4*duration + 4 < t < 2 + 5*duration + 2:
-			arcade.draw_text("NES", 0.5 * Globals.WIDTH, Globals.HEIGHT - 82, color=GREEN, font_size=60,
-		                 anchor_x="center")
+			arcade.draw_text("NES", x, y, color=GREEN, font_size=60, anchor_x="center")
+			self.draw_pic(self.nes)
 
 		elif 2 + 5*duration + 4 < t < 2 + 6*duration + 2:
-			arcade.draw_text("Playstation", 0.5 * Globals.WIDTH, Globals.HEIGHT - 82, color=AQUA, font_size=60,
-		                 anchor_x="center")
+			arcade.draw_text("Playstation", x, y, color=AQUA, font_size=60, anchor_x="center")
 
 		elif 2 + 6*duration + 4 < t < 2 + 7*duration + 2:
-			arcade.draw_text("VIC-20", 0.5 * Globals.WIDTH, Globals.HEIGHT - 82, color=ARCADE_YELLOW, font_size=60,
-		                 anchor_x="center")
+			arcade.draw_text("VIC-20", x, y, color=ARCADE_YELLOW, font_size=60, anchor_x="center")
 
-		elif  2 + 9*duration + 2 < t < 12.5 * duration:
-			arcade.draw_text("Pixelove OLE", 0.5 * Globals.WIDTH, Globals.HEIGHT - 82, color=BLACK, font_size=77,
-		                 anchor_x="center")
-			arcade.draw_text("23.07.2026", 0.5 * Globals.WIDTH, Globals.HEIGHT - 192, color=BLACK, font_size=99,
-		                 anchor_x="center")
-			arcade.draw_text("Lotnisko w Spalicach", 0.5 * Globals.WIDTH, Globals.HEIGHT//3, color=BLACK, font_size=66,
-		                 anchor_x="center")
+		elif 2 + 9*duration + 2 < t < 12.5 * duration:
+			arcade.draw_text("Pixelove OLE", x, y-10, color=BLACK, font_size=77, anchor_x="center")
+			arcade.draw_text("23.07.2026", x, Globals.HEIGHT - 202, color=BLACK, font_size=99
+			                 , anchor_x="center")
+			arcade.draw_text("Lotnisko w Spalicach", x, Globals.HEIGHT // 2.5, color=BLACK
+			                 , font_size=66,anchor_x="center")
 
 		elif t > 13*duration:
 			dur = datetime.now() - Globals.start
 			print("Invitro duration: " + str(dur))
-			sys.exit(0)
+
+			print("Restarting...")
+			python = sys.executable
+			os.execl(python, python, *sys.argv)
+
+	def draw_pic(self, pic):
+		rect = Rect(
+			x=Globals.WIDTH // 2,
+			y=Globals.HEIGHT // 2,
+			width=pic.width,
+			height=pic.height,
+			left=Globals.WIDTH // 2,
+			top=Globals.HEIGHT // 2,
+			right=Globals.WIDTH // 2,
+			bottom=Globals.HEIGHT // 2
+		)
+		arcade.draw_texture_rect(
+			rect=rect,
+			texture=pic,
+			angle=0.0,
+			color=WHITE,
+			alpha=255,
+			pixelated=False
+		)
